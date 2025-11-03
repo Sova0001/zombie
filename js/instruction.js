813cc15e549a92c3cc5dc9a523785b16b02c7990
Sofie <sova0001@stud.ek.dk>
2025-11-03 08:40:54 +0100

    update

--- js/instruction.js
+++ js/instruction.js
@@ -1,6 +1,6 @@
 console.log("Loaded instructions");
 
-document.querySelector("mund").addEventListener("click", mundF);
+document.querySelector("#mund").addEventListener("click", mundF);
 
 function mundF() {
   console.log("mundF er trykket paa");
